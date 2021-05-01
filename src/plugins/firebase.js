2cda0ea24e989f5f68e73c756ce5e7eef73029f6
takuma-ru <kcattdev@gmail.com>
2021-05-01 14:30:59 +0900

    Update firebase.js

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDERID,
-  appId: process.env.VUE_APP_APP_ID,
-  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+  apiKey: secrets.VUE_APP_API_KEY,
+  authDomain: secrets.VUE_APP_AUTH_DOMAIN,
+  databaseURL: secrets.VUE_APP_DATABASE_URL,
+  projectId: secrets.VUE_APP_PROJECT_ID,
+  storageBucket: secrets.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: secrets.VUE_APP_MESSAGING_SENDERID,
+  appId: secrets.VUE_APP_APP_ID,
+  measurementId: secrets.VUE_APP_MEASUREMENT_ID
 };
 firebase.initializeApp(firebaseConfig);
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
